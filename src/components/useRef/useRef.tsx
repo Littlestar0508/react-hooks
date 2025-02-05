20b994e4f061ce2f7e0f7e1f774642978602caa4
Littlestar0508 <rist9860@gmail.com>
2025-02-05 22:59:01 +0900

    input 태그 value 임의 조작

--- src/components/useRef/useRef.tsx
+++ src/components/useRef/useRef.tsx
@@ -2,22 +2,29 @@ import { useRef } from 'react';
 import module from './useRef.module.css';
 
 export default function UseRef() {
-  const timer = useRef(0);
+  // const timer = useRef(0);
 
-  const handleChangeTime = () => {
-    timer.current += 100;
-    setTimeout(() => {
-      alert(`지금 걸린 시간은 ${timer.current}ms 만큼 걸렸다`);
-    }, timer.current);
+  // const handleClick = () => {
+  //   timer.current += 100;
+  //   setTimeout(() => {
+  //     alert(`지금 걸린 시간은 ${timer.current}ms 만큼 걸렸다`);
+  //   }, timer.current);
+  // };
+
+  const inputRef = useRef(null);
+
+  const handleClick = () => {
+    if (inputRef.current) {
+      (inputRef.current as HTMLInputElement).value = '안녕하세요~';
+    }
   };
 
   return (
-    <button
-      type="button"
-      className={module['use-ref']}
-      onClick={handleChangeTime}
-    >
-      시간 늘리기
-    </button>
+    <>
+      <button type="button" className={module['use-ref']} onClick={handleClick}>
+        useRef
+      </button>
+      <input type="text" ref={inputRef} />
+    </>
   );
 }
