3f3ffbb3188d2e9ea97a54eecb480c0dbc41526c
Littlestar0508 <rist9860@gmail.com>
2025-02-05 22:41:04 +0900

    useRef를 이용한 alert창 지연 시간 늘리기

--- src/components/useRef/useRef.tsx
+++ src/components/useRef/useRef.tsx
@@ -1,3 +1,23 @@
+import { useRef } from 'react';
+import module from './useRef.module.css';
+
 export default function UseRef() {
-  return <div>ㅎㅇ</div>;
+  const timer = useRef(0);
+
+  const handleChangeTime = () => {
+    timer.current += 100;
+    setTimeout(() => {
+      alert(`지금 걸린 시간은 ${timer.current}ms 만큼 걸렸다`);
+    }, timer.current);
+  };
+
+  return (
+    <button
+      type="button"
+      className={module['use-ref']}
+      onClick={handleChangeTime}
+    >
+      시간 늘리기
+    </button>
+  );
 }
